01cbb71cb498c9b152c8d0a2ecc08d298da7d1d8
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-03-07 08:26:13 +0900

    :bento: re-coloring assets

--- Targets/MyApp/Sources/Presentation/Auth/SignIn/VC/SignInVC.swift
+++ Targets/MyApp/Sources/Presentation/Auth/SignIn/VC/SignInVC.swift
@@ -13,33 +13,23 @@ final class SignInVC: BaseVC<SignInVM>, Stepper{
     
     private let mainLogoImageView = UIImageView(image: UIImage(named: "Paper_MainLogo")!)
     
-    private let welcomeTextView = UITextView().then{
+    private let welcomeLabel = UILabel().then{
         $0.text = "다시 온걸 환영해요!"
-        $0.font = UIFont(name: "Helvetica-Bold", size: 14)
+        $0.font = UIFont(name: "Helvetica-Bold", size: 20)
         $0.textColor = UIColor.white
-        $0.backgroundColor = GlogAsset.Colors.paperBackgroundColor.color
-        $0.isEditable = false
-        $0.isSelectable = false
-    }
-    
-    private let backButton = UIButton().then{
-        //image
-        $0.layer.backgroundColor = UIColor.white.cgColor
     }
     
     private let idTextField = UITextField().then{
-        $0.placeholder = "아이디"
         $0.attributedPlaceholder = NSAttributedString(string: "아이디를 입력해주세요.", attributes: [
             NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20.0, weight: .medium), NSAttributedString.Key.foregroundColor : UIColor.gray.cgColor
         ])
         $0.layer.backgroundColor = GlogAsset.Colors.paperBlankColor.color.cgColor
         $0.textColor = UIColor.white
-        $0.allowsEditingTextAttributes = true
+        $0.layer.cornerRadius = 10
         $0.font = UIFont(name: "Helvetica", size: 20)
     }
     
     private let pwdTextField = UITextField().then{
-        $0.placeholder = "비밀번호"
         $0.attributedPlaceholder = NSAttributedString(string: "비밀번호를 입력해주세요.", attributes: [
             NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20.0, weight: .medium), NSAttributedString.Key.foregroundColor : UIColor.gray.cgColor
         ])
@@ -72,8 +62,7 @@ final class SignInVC: BaseVC<SignInVM>, Stepper{
     
     override func addView(){
         [mainLogoImageView,
-         welcomeTextView,
-         backButton,
+         welcomeLabel,
          idTextField,
          pwdTextField,
          signInButton,
@@ -86,27 +75,22 @@ final class SignInVC: BaseVC<SignInVM>, Stepper{
     
     override func setLayout(){
         mainLogoImageView.snp.makeConstraints { make in
+            make.left.equalTo(36)
+            make.top.equalTo(150)
+            make.width.equalTo(277)
+            make.height.equalTo(96)
+        }
+        welcomeLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalTo(30)
-            make.width.equalTo(170)
-            make.height.equalTo(100)
-        }
-        welcomeTextView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview().offset(25)
-            make.top.equalTo(mainLogoImageView.snp.bottom).offset(10)
-            make.width.equalTo(154)
+            make.top.equalTo(mainLogoImageView.snp.bottom).offset(20)
+            make.width.equalTo(160)
             make.height.equalTo(24)
-        }
-        backButton.snp.makeConstraints { make in
-            make.top.equalTo(10)
-            make.left.equalTo(10)
-            make.size.equalTo(20)
         }
         idTextField.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.width.equalToSuperview().inset(15)
             make.height.equalTo(35)
-            make.top.equalTo(welcomeTextView.snp.bottom).offset(20)
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(20)
         }
         pwdTextField.snp.makeConstraints { make in
             make.centerX.equalTo(idTextField)
@@ -124,6 +108,4 @@ final class SignInVC: BaseVC<SignInVM>, Stepper{
             make.top.equalTo(signInButton.snp.bottom).offset(15)
         }
     }
-    
-    
 }
