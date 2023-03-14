4e6ba35a37de6f4ba731d1736cee7d4aa95f17f3
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-03-14 10:04:23 +0900

    :sparkles: gif

--- Targets/MyApp/Sources/Presentation/Main/VC/MainVC.swift
+++ Targets/MyApp/Sources/Presentation/Main/VC/MainVC.swift
@@ -5,24 +5,57 @@ import RxCocoa
 import SnapKit
 import Alamofire
 import UIKit
+import Gifu
 
 final class MainVC: BaseVC<MainVM>{
+    
+    private let mainLabel = UILabel().then{
+        $0.text = "너의 생각을 글로 표현해봐!"
+        $0.textColor = .white
+        $0.font = UIFont.systemFont(ofSize: 40, weight: .bold)
+        $0.numberOfLines = 0
+        $0.lineBreakMode = .byWordWrapping
+    }
+    
+    let gifImage = GIFImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override func setup() {
+        DispatchQueue.main.async {
+            self.gifImage.animate(withGIFNamed: "Paper_Smile", animationBlock: {})
+        }
+    }
+    
     override func addView() {
-        
+        [mainLabel,
+        gifImage
+        ].forEach{
+            view.addSubview($0)
+        }
     }
     
     override func configureNavigation() {
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: GlogAsset.Images.paperMainLogo.image, style: .plain, target: self, action: .none)
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: GlogAsset.Images.paperProfileLogo.image.downsam, style: .plain, target: self, action: .none)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: GlogAsset.Images.paperMainLogo.image.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: .none)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: GlogAsset.Images.paperProfileLogo.image.downSample(size: .init(width: 36, height: 36)).withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: .none)
     }
     
     override func setLayout() {
+        mainLabel.snp.makeConstraints { make in
+            make.top.equalTo(123)
+            make.left.equalTo(12)
+            make.width.equalTo(230)
+            make.height.equalTo(98)
+        }
         
+        gifImage.snp.makeConstraints { make in
+            make.centerY.equalTo(mainLabel)
+            make.right.equalTo(12)
+            make.height.equalTo(135)
+            make.width.equalTo(170)
+        }
     }
     
     @objc func profileButtonDidTap(){
