65986141209bd99fc870585a089327ec4c106f3e
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-03-17 14:31:43 +0900

    :sparkles: post list service

--- Targets/MyApp/Sources/APIService/Service/FeedService.swift
+++ Targets/MyApp/Sources/APIService/Service/FeedService.swift
@@ -38,15 +38,15 @@ extension FeedService: TargetType{
         switch self {
         case .hot:
             return .requestPlain
-        case .postList:
-            return .
+        case let .postList(keyword):
+            return .requestParameters(parameters: ["size": keyword.size, "page": keyword.page, "keyword": keyword.keyword], encoding: URLEncoding.queryString)
         }
     }
     
     var headers: [String : String]?{
         let tk = Keychain()
         switch self {
-        case .hot:
+        case .hot, .postList:
             return ["Authorization" : tk.read(key: "accessToken")!]
         default:
             return["Content-Type" : "application/json"]
