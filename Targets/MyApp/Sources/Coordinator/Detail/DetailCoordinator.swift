2a61489fa7d9228539e36fb7c004e15d8b6ea6b1
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-04-11 12:13:36 +0900

    :fire: 걍 없애기

--- Targets/MyApp/Sources/Coordinator/Detail/DetailCoordinator.swift
+++ Targets/MyApp/Sources/Coordinator/Detail/DetailCoordinator.swift
@@ -8,4 +8,22 @@ class DetailCoordinator: BaseCoordinator{
         
         navigationController.pushViewController(vc, animated: true)
     }
+    
+    override func navigate(to step: GlogStep) {
+        switch step{
+        case let .myPageIsRequired(model):
+            return myPageIsRequired(model: model)
+        default:
+            return
+        }
+    }
+}
+
+extension DetailCoordinator{
+    private func myPageIsRequired(model: UserProfileResponse){
+        let vc = MyPageCoordinator(navigationController: navigationController)
+        vc.parentCoordinator = self
+        childCoordinators.append(vc)
+        vc.startMyPageVC(model: model)
+    }
 }
--- Targets/MyApp/Sources/Presentation/Detail/VC/DetailVC.swift
+++ Targets/MyApp/Sources/Presentation/Detail/VC/DetailVC.swift
@@ -103,7 +103,6 @@ final class DetailVC: BaseVC<DetailVM>{
     
     override func configureNavigation() {
         self.navigationItem.titleView = UIImageView(image: GlogAsset.Images.paperMainLogo.image.downSample(size: .init(width: 26, height: 26)).withRenderingMode(.alwaysOriginal))
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: GlogAsset.Images.paperProfileLogo.image.downSample(size: .init(width: 36, height: 36)).withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: .none)
         let standardAppearance = UINavigationBarAppearance()
         standardAppearance.configureWithOpaqueBackground()
         standardAppearance.backgroundColor = GlogAsset.Colors.paperBackgroundColor.color
