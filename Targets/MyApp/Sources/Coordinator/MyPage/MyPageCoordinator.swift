57dfe140a60243310ef255c5e39a135f99b23383
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-04-17 17:32:23 +0900

    :ambulance: mypage 에서 게시물 들어가기

--- Targets/MyApp/Sources/Coordinator/MyPage/MyPageCoordinator.swift
+++ Targets/MyApp/Sources/Coordinator/MyPage/MyPageCoordinator.swift
@@ -8,4 +8,22 @@ class MyPageCoordinator: BaseCoordinator{
         
         navigationController.pushViewController(vc, animated: true)
     }
+    
+    override func navigate(to step: GlogStep) {
+        switch step{
+        case .detailIsRequired(let model):
+            detailIsRequired(model: model)
+        default:
+            return
+        }
+    }
+}
+
+private extension MyPageCoordinator{
+    private func detailIsRequired(model: DetailResponse){
+        let vc = DetailCoordinator(navigationController: navigationController)
+        vc.parentCoordinator = self
+        childCoordinators.append(vc)
+        vc.startDetailPostVC(model: model)
+    }
 }
